ef0aa8352e3342c1f9979fd20f47f172d8fed440
linzhengen <linzhengen@yahoo.co.jp>
2021-11-25 22:05:35 +0900

    add tools

--- shell.sh
+++ shell.sh
@@ -2,3 +2,4 @@ sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools
 
 curl -s -S -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
 
+softwareupdate --install-rosetta
